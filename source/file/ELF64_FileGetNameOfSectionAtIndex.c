5a4cd25830d6c50f64db747fe2549db892ac5e3b
macmade <macmade@xs-labs.com>
2015-02-24 01:46:48 +0100

    Added the new copyright notices.

--- source/file/ELF64_FileGetNameOfSectionAtIndex.c
+++ source/file/ELF64_FileGetNameOfSectionAtIndex.c
@@ -59,6 +59,11 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
 
+/*!
+ * @author          Jean-David Gadina
+ * @copyright       (c) 2010-2015, Jean-David Gadina - www.xs-labs.com
+ */
+
 #include <elf.h>
 #include <elf/__private/elf.h>
 #include <stdlib.h>
